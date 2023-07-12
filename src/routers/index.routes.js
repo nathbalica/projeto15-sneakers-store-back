3426270dfb2e3703cd840b49bf239aae9cead294
nathbalica <nathbalica@gmail.com>
2023-07-12 11:08:19 -0300

    fix: :bug: add .js in imports
    app crashed - waiting for file changes before starting because imports

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
-import authRouter from "./auth.routes"
-import orderRouter from "./order.routes"
-import productsRouter from "./products.routes"
-import shoppingCartRouter from "./shoppingCart.routes"
+import authRouter from "./auth.routes.js"
+import orderRouter from "./order.routes.js"
+import productsRouter from "./products.routes.js"
+import shoppingCartRouter from "./shoppingCart.routes.js"
 
 const router = Router()
 
@@ -11,4 +11,4 @@ router.use(orderRouter)
 router.use(productsRouter)
 router.use(shoppingCartRouter)
 
-export default router
+export default router;
