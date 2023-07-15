d9c83239acb937ff8d2d6f04c771ddcf7a2a5b9c
pablodru <pdrocha03@gmail.com>
2023-07-15 14:14:48 -0300

    feat: rotas da página de checkout

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,49 @@
+import { db } from "../database/db.js";
+import { ObjectId } from "mongodb";
+
+export async function getOrder ( req, res ) {
+    const { userId } = res.locals.session;
+
+    try{
+
+        const userCart = await db.collection("order").findOne({ userId }); //VERIFICAR SE É USERID MESMO O NOME DA PROPRIEDADE USADA
+        const items = userCart.itemsId.map(async id => {
+            try{
+                const item = await db.collection("products").findOne({ _id: new ObjectId(id) });
+                return item;
+            } catch (err) {
+                return res.status(500).send(err.message);
+            }
+        })
+
+        return res.status(200).send(items);
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export async function closeOrder ( req, res ) {
+    const { userId } = res.locals.session
+
+    try{
+
+        await db.collection("order").deleteOne({ userId }); //VERIFICAR SE É USERID MESMO O NOME DA PROPRIEDADE USADA
+        await db.collection("cart").deleteMany({ userId });
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export async function cancelOrder ( req, res ) {
+    const { userId } = res.locals.session;
+
+    try{
+
+        await db.collection("order").deleteOne({ userId });
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
--- src/routers/order.routes.js
+++ src/routers/order.routes.js
@@ -1,6 +1,12 @@
-import { Router } from "express"
+import { Router } from "express";
+import { cancelOrder, closeOrder, getOrder } from "../controllers/order.controller";
+import { authValidation } from "../middlewares/authValidation.middleware";
 
 
 const orderRouter = Router()
+
+orderRouter.get('/checkout', authValidation, getOrder);
+orderRouter.delete('/checkout', authValidation, closeOrder);
+orderRouter.delete('/cancel-checkout', authValidation, cancelOrder);
 
 export default orderRouter
