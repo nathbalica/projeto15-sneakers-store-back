a7a2ffe8b1f729304c7cc57dae2b717032917ee2
thiago-arend <thiago.arend@gmail.com>
2023-07-15 20:14:31 -0300

    fix: apresentação de erro 404 quando não existe o id do produto

--- src/controllers/shoppingCart.controller.js
+++ src/controllers/shoppingCart.controller.js
@@ -4,9 +4,12 @@ import { db } from "../database/db.js";
 export async function insertProductIntoCart(req, res) {
     const { userId, item } = req.body;
 
-    if (!userId || !item) return res.sendStatus(422);
+    if (!userId || !item) return res.status(422).send("Preencha o id do item");
 
     try {
+
+        const productExists = await db.collection("products").findOne({_id: new ObjectId(item)});
+        if (!productExists) return res.status(404).send("O produto não existe!");
 
         const update = await db.collection("cart").updateOne({ userId }, { $push: { itens: item } });
         if (update.matchedCount === 0) { // se não encontrou para atualizar
