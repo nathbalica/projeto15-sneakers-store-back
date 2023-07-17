8a61a831dd800466d573d7297e93e41413b4f8ca
thiago-arend <thiago.arend@gmail.com>
2023-07-16 22:39:50 -0300

    fix: palavra errada gerando erros no controller da rota do produto

--- src/routers/shoppingCart.routes.js
+++ src/routers/shoppingCart.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getCart, insertOneIntoCart, removeAllById, removeAllFromCart, removeOneFromCart } from "../controllers/shoppingCart.controller.js";
+import { getCart, insertOneIntoCart, removeAllFromCart, removeOneFromCart } from "../controllers/shoppingCart.controller.js";
 
 
 const shoppingCartRouter = Router();
@@ -8,6 +8,5 @@ shoppingCartRouter.post("/cart", insertOneIntoCart);
 shoppingCartRouter.get("/cart/:id", getCart);
 shoppingCartRouter.post("/cart/:id", removeOneFromCart);
 shoppingCartRouter.put("/cart/:id", removeAllFromCart);
-shoppingCartRouter.put("/cart/:id/:itemId", removeAllById);
 
 export default shoppingCartRouter;
